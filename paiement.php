4561783fc74690bfe38b40b14236bf4455d91b5e
Albin RIVIERE <riviere.albin@gmail.com>
2024-09-18 12:51:31 +0200

    first

---
+++ paiement.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="paiement.css">
+    <title>Paiement</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="images/IL_PIATTO.png" alt="Logo" width="25px" height="25px" class="d-inline-block align-text-top">
+            il piatto
+          </a>
+        </div>
+      </nav>
+    <center><h1 style="margin-top: 10%; color:aliceblue;" >VOTRE COMMANDE A ETE EFFECTUEE</h1></center>
+    <center><button class="button-back-acceuil " type="button" style="margin-top: 20%;"> <strong> Retour à l'acceuil </strong></button></center>
+
+</body>
+</html>
