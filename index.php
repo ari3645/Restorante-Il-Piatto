01e0025adf803984f0f62c201a0d9ca007145aec
Albin RIVIERE <riviere.albin@gmail.com>
2024-09-18 11:25:28 +0200

    first

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IL PIATTO</title>
-    <link rel="stylesheet" href="commande.css">
-    <link rel="icon" href="Images/IL PIATTO.png">
+    <link rel="stylesheet" href="accueil.css">
+    <link rel="icon" href="Image/IL PIATTO.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
@@ -15,43 +15,39 @@
                 <p class="yeseva-one-regular">IL PIATTO</p>
             </div>
             <div class="zoneboutons">
-                <button class="btn btn-nav reserver">Réserver</button>
-                <button class="btn btn-nav commander">Commander</button>
+                <button class="btn btn-nav reserver" onclick="window.location.href='reservation.php';">Réserver</button>
+                <button class="btn btn-nav commander" onclick="window.location.href='commande.php';">Commander</button>
             </div>
 
     </nav>
 
     <div class="container">
-        <img src="Images/nuage1.png" class="nuage1">
-        <div class="zone2">
-            Mon texte
-                     
-        </div>
+        <h1 class="titre1">Nos<br>incontournables</h1>
                 <div class="slider-container">
                     <button id="prev" class="prev-btn"><</button>
                     <div class="slider">
                         <div class="card plats">
-                        <img src="Images/Pizza.png">
+                        <img src="Image/Pizza.png">
                             Nos pizzas
                         </div>
                         <div class="card plats">
-                            <img src="Images/Carbonara.png">
+                            <img src="Image/Carbonara.png">
                             Nos pâtes
                         </div>
                         <div class="card">
-                            <img src="Images/Tiramisu.png">
+                            <img src="Image/Tiramisu.png">
                             Nos desserts
                         </div>
                         <div class="card">
-                            <img src="Images/Café.png">
+                            <img src="Image/Café.png">
                             Nos digestifs
                         </div>
                         <div class="card">
-                            <img src="Images/Lasagna.png">
+                            <img src="Image/Lasagna.png">
                             Nos plats
                         </div>
                         <div class="card">
-                            <img src="Images/Salade.png">
+                            <img src="Image/Salade.png">
                             Nos salades
                         </div>
                     </div>
@@ -59,25 +55,40 @@
                 </div>
 
                 <div class="zonemenu">
-                    <button class="btn btn-menu">Menu</button>
+                    <button class="btn btn-menu">Accéder au menu</button>
                 </div>
                     
-                <div class="zonetexte">
-                    <p>
-                        Il Piatto est un petit restaurant familial dont la production est perpétuée de génération en génération depuis 1968.
-                        Nous sommes implantés à Lyon, au 53 cours Albert Thomas.
-                        Notre établissement propose des spécialités culinaires originaires d’Italie.
-                        A partir de 23h, nous proposons également, une fois par semaine des évènements à thèmes, au sein du restaurant.
-                    </p>
+                <div class="row zonetexte2">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-4">
+                        <div class="zonetexte">
+                            <p>
+                                Il Piatto est un petit restaurant familial dont la production est perpétuée de génération en génération depuis 1968.
+                                Nous sommes implantés à Lyon, au 53 cours Albert Thomas.
+                                <br>Notre établissement propose des spécialités culinaires originaires d’Italie.
+                                A partir de 23h, nous proposons également, une fois par semaine des évènements à thèmes, au sein du restaurant.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-2"></div>
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="titre2">
+                                <h2>Halloween</h2>
+                            </div>
+                            <h3>31 octobre</h3>
+                            <p>Soirée échange de friandises + déguisements exigés</p>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="zonehoraires">
 
                 </div>
 
-                <div class="zoneboutons">
-                    <button class="btn btn-nav reserver">Réserver</button>
-                    <button class="btn btn-nav commander">Commander</button>
+                <div class="zoneboutons zoneboutons2">
+                    <button class="btn btn-nav reserver" onclick="window.location.href='reservation.php';">Réserver</button>
+                    <button class="btn btn-nav commander" onclick="window.location.href='commande.php';">Commander</button>
                 </div>
 
                 <div class="zoneplanning">
@@ -85,14 +96,46 @@
                         <h1>Notre planning</h1>
                         <div class="zoneevenements">
                             <div class="card">
-                                <h2>Halloween</h2>
+                                <div class="titre2">
+                                    <h2>Halloween</h2>
+                                </div>
                                 <h3>31 octobre</h3>
                                 <p>Soirée échange de friandises + déguisements exigés</p>
                             </div>
-                            <div class="card"></div>
-                                <h2>Noël</h2>
+                            <div class="card">
+                                <div class="titre2">
+                                    <h2>Noël</h2>
+                                </div>
                                 <h3>24 décembre</h3>
                                 <p>Soirée d'ambiance festive + père-noël</p>
+                            </div>
+                            <div class="card">
+                                <div class="titre2">
+                                    <h2>Nouvelle année</h2>
+                                </div>
+                                <h3>31 décembre</h3>
+                                <p>Soirée festive + décompte jusqu'à minuit</p>
+                            </div>
+                            <div class="card">
+                                <div class="titre2">
+                                    <h2>Epiphanie</h2>
+                                </div>
+                                <h3>6 janvier</h3>
+                                <p>Galette des rois offerte pour chaque consommation de plus de 150€</p>
+                            </div>
+                            <div class="card">
+                                <div class="titre2">
+                                    <h2>Paques</h2>
+                                </div>
+                                <h3>19 avril</h3>
+                                <p>Chasse aux oeufs dans tout le restaurant</p>
+                            </div>
+                            <div class="card">
+                                <div class="titre2">
+                                    <h2>Saint Valentin</h2>
+                                </div>
+                                <h3>14 février</h3>
+                                <p>Soirée à ambiance romantique + boisson spéciale</p>
                             </div>
                         </div>
                     </div>
